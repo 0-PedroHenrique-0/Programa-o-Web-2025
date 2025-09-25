e2bdd7ad3e0a0825af28667fb17f33b2839f6734
PEDRO HENRIQUE TEIXEIRA SILVA <pedro.htsilva9@senacsp.edu.br>
2025-09-24 21:20:27 -0300

    aula

---
+++ Aula24.09/script.js
@@ -0,0 +1,14 @@
+document.getElementById('btnEnviar').innerText = 'Enviar para Nasa';
+for( let i=0; i< 1000; i++)
+    console.log (i);
+
+{
+   alert("teste 123")
+    if (i == 0){
+    }
+    else ( i == 1)
+    {
+        console.log ('Estou te Rackeando');
+    }    console.log (i);
+    
+}
